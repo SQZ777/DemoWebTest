ac9c046e087c3fe460a37b07a6dc6cd5dba7a8dd
sqz777 <sqz777isme@gmail.com>
2017-09-20 00:38:57 +0800

    update content about click and comment

--- src/DemoWebTest.java
+++ src/DemoWebTest.java
@@ -9,10 +9,9 @@ public class DemoWebTest {
 		WebDriver chromeDriver = new ChromeDriver();      //建立一個WebDriver物件
 		chromeDriver.manage().window().maximize();        //開啟的視窗要放大 才比較好讓我們觀看執行的過程
 		chromeDriver.get("https://www.google.com/");      //開啟google首頁
-		chromeDriver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("1+1");                     //針對搜尋列輸入1+1
-		chromeDriver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[3]/center/input[1]")).submit(); //針對按鈕進行Submit動作
+		chromeDriver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("1+1");                     //針對這個xpath輸入1+1
+		chromeDriver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[3]/center/input[1]")).submit(); //針對這個xpath進行Submit動作
+		chromeDriver.findElement(By.xpath("//*[@id=\"rso\"]/div[2]/div/div[1]/div/div/h3/a")).click(); //針對這個xpath做點選動作
 		Thread.sleep(3000);  //避免一完成就直接關閉 所以讓他暫時停止3秒
 		chromeDriver.quit(); //關閉視窗
 	}
-
-}
